bee8bc668cd7096478b2570da69f790c3f76a219
Hassankashi <mahsa.hassankashi@gmail.com>
2015-03-07 02:17:38 +0330

    NH

---
+++ blogit.dal/CreateDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blogit.dal.Entities;
+
+namespace blogit.dal
+{
+    class CreateDB
+    {
+        public void Database()
+        {
+
+            using (var session = blogit.dal.NHibernatecfg.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var ArticleObj = new Article { Name = "BlogIT" };
+                    session.Save(ArticleObj);
+                    transaction.Commit();
+                    //Console.WriteLine("Article was inserted : " + ArticleObj.Name);
+                }
+
+            } 
+    
+        }
+    }
+}
