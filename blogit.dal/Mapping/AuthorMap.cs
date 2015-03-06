0122705aa2f21c347876e5ebdb91c4426ea39779
Hassankashi <mahsa.hassankashi@gmail.com>
2015-03-07 01:42:12 +0330

    NH

---
+++ blogit.dal/Mapping/AuthorMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using blogit.dal.Entities;
+
+namespace blogit.dal.Mapping
+{
+    class AuthorMap: ClassMap<Author>
+    {
+        //Constructor
+        public AuthorMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name);
+
+            References(x => x.AuthorArticle).Column("ArticleId");
+
+            Table("Author");
+        }
+    }
+}
