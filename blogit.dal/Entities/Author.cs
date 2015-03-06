09899f848c2c287c083762cb7986bede28de304c
Hassankashi <mahsa.hassankashi@gmail.com>
2015-03-07 01:41:43 +0330

    NH

---
+++ blogit.dal/Entities/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blogit.dal.Entities
+{
+    class Author
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual Article AuthorArticle { get; set; }
+    }
+}
