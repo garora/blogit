f94027f4d0d73c1fb4e0aac351fcf054df45ae34
Hassankashi <mahsa.hassankashi@gmail.com>
2015-03-07 01:39:04 +0330

    NH

---
+++ blogit.web/Controllers/BuildDBController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using blogit.dal;
+
+namespace blogit.web.Controllers
+{
+    public class BuildDBController : Controller
+    {
+        // GET: BuildDB
+        public ActionResult Index()
+        {
+            using (var session = blogit.dal.NHibernatecfg.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var CountryObj = new Country { CountryName = "IRAN" };
+                    session.Save(CountryObj);
+                    transaction.Commit();
+                    Console.WriteLine("Country was inserted : " + CountryObj.CountryName);
+                }
+
+            } 
+            return View();
+        }
+    }
+}
