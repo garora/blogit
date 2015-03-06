22c4246ee1a30813d921652913fddde91a47447e
Hassankashi <mahsa.hassankashi@gmail.com>
2015-03-07 01:41:58 +0330

    NH

---
+++ blogit.dal/Mapping/ArticleMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using blogit.dal.Entities;
+
+namespace blogit.dal.Mapping
+{
+    class ArticleMap : ClassMap<Article>
+    {
+        public ArticleMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name);
+            
+            Map(x => x.Body);
+
+            Table("Article");
+        }
+    }
+}
