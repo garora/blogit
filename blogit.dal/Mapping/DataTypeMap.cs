3943b6d94c1a8b83cbd07788fbe4433da2668c67
Hassankashi <mahsa.hassankashi@gmail.com>
2015-04-04 15:13:17 +0430

    nh

---
+++ blogit.dal/Mapping/DataTypeMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using BlogIT.Dal.Entities;
+
+namespace BlogIT.Dal.Mapping
+{
+    public class DataTypeMap : ClassMap<DataType>
+    {
+        public DataTypeMap()
+        {
+            Id(x => x.ID);
+
+            Map(x => x.Name);
+
+            Table("DataType");
+        }
+    }
+}
