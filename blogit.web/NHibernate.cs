1f5dda0ddeb99b107984ee2bf8eac5222449fd29
Hassankashi <mahsa.hassankashi@gmail.com>
2015-04-04 14:48:06 +0430

    mh

---
+++ blogit.web/NHibernate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using BlogIT.Dal.Entities;     //ProjectName.Entites
+using BlogIT.Web.Controllers; //ProjectName.Controllers
+
+namespace BlogIT.Web
+{
+    public class NHibernate
+    {
+        private static ISessionFactory _sessionFactory;
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                    InitializeSessionFactory();
+                return _sessionFactory;
+            }
+        }
+
+        private static void InitializeSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+            .Database(MsSqlConfiguration.MsSql2008
+                          .ConnectionString(
+                              @"Server=.;initial catalog=BlogIT;integrated security=True")
+                          .ShowSql()
+            )
+            .Mappings(m =>
+                      m.FluentMappings
+                          .AddFromAssemblyOf<GenerateDBController>())
+            .ExposeConfiguration(cfg => new SchemaUpdate(cfg)
+                                            .Execute(false, true))
+            .BuildSessionFactory();
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
