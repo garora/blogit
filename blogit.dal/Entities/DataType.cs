f32651a76c4a45b8f5c27a9a6f5d93e68bd03aba
Hassankashi <mahsa.hassankashi@gmail.com>
2015-04-04 15:12:18 +0430

    nh

---
+++ blogit.dal/Entities/DataType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogIT.Dal.Entities
+{
+    public class DataType
+    {
+        public virtual int ID { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
